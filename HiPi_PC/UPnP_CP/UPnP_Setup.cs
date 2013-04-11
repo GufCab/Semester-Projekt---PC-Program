3c615740521d511e4a49232bb454410a3646010c
Christian Svejstrup <chrmaui@gmail.com>
2013-04-11 17:39:23 +0200

    Added gui to CP

---
+++ HiPi_PC/UPnP_CP/UPnP_Setup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OpenSource.DeviceBuilder;
+using OpenSource.UPnP;
+
+namespace UPnP_CP
+{
+    public class UPnP_Setup
+    {
+        private static MediaRendererDiscovery _SinkDisco;
+        private static MediaServerDiscovery _SourceDisco;
+        /*
+        public static SinkStack.CpAVTransport _AVTransport;
+        public static SinkStack.CpConnectionManager _ConnectionManager;
+        public static SinkStack.CpRenderingControl _RenderingControl;
+        */
+
+
+
+        public static void StartSourceDisco()
+        {
+            _SourceDisco = new MediaServerDiscovery();
+            _SourceDisco.OnAddedDevice += new MediaServerDiscovery.DiscoveryHandler(AddSource);
+            _SourceDisco.OnRemovedDevice += new MediaServerDiscovery.DiscoveryHandler(RemoveSource);
+            _SourceDisco.Start();
+        }
+
+        public static void StartSinkDisco()
+        {
+            _SinkDisco = new MediaRendererDiscovery();
+            _SinkDisco.OnAddedDevice += new MediaRendererDiscovery.DiscoveryHandler(AddSink);
+            _SinkDisco.OnRemovedDevice += new MediaRendererDiscovery.DiscoveryHandler(RemoveSink);
+            _SinkDisco.Start();
+
+        }
+
+        private static void AddSink(MediaRendererDiscovery sender, UPnPDevice d)
+        {
+            Console.WriteLine("Added Device: " + d.FriendlyName);
+
+                UPnP_Functions._AVTransport = new SinkStack.CpAVTransport(d.GetServices(SinkStack.CpAVTransport.SERVICE_NAME)[0]);
+                UPnP_Functions._ConnectionManager = new SinkStack.CpConnectionManager(d.GetServices(SinkStack.CpConnectionManager.SERVICE_NAME)[0]);
+                UPnP_Functions._RenderingControl = new SinkStack.CpRenderingControl(d.GetServices(SinkStack.CpRenderingControl.SERVICE_NAME)[0]);
+        }
+
+        private static void RemoveSink(MediaRendererDiscovery sender, UPnPDevice d)
+        {
+            Console.WriteLine("Removed Device: " + d.FriendlyName);
+        }
+
+
+        private static void AddSource(MediaServerDiscovery sender, UPnPDevice dev)
+        {
+            //Todo: Implement Stack reception with wrappers
+            throw new NotImplementedException();
+        }
+
+        private static void RemoveSource(MediaServerDiscovery sender, UPnPDevice dev)
+        {
+            //Todo: Remove device in some way
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ HiPi_PC/UPnP_CP_GUI/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UPnP_CP;
+
+namespace UPnP_CP_GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private UPnP_Functions _UPnPFunctions;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            UPnP_Setup.StartSinkDisco();
+            _UPnPFunctions = new UPnP_Functions();
+        }
+      
+        private void btnPlayInvoke_Click(object sender, RoutedEventArgs e)
+        {
+            _UPnPFunctions.Play();
+        }
+
+        private void btnPauseInvoke_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void btnStopInvoke_Click(object sender, RoutedEventArgs e)
+        {
+          
+        }
+
+        private void btnNextInvoke_Click(object sender, RoutedEventArgs e)
+        {
+         
+        }
+
+        private void btnPreviousInvoke_Click(object sender, RoutedEventArgs e)
+        {
+         
+        }
+
+        private void btnVolume_Click(object sender, RoutedEventArgs e)
+        {
+         
+        }
+
+        private void btnSetTransportURI_Click(object sender, RoutedEventArgs e)
+        {
+          
+        }
+
+        private void btnSubscribe_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+    }
+}
