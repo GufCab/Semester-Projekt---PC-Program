f91a1d79293aa933c84e968b63e08c0d541e8bc1
Michael Oksen <cronix1990@gmail.com>
2013-04-29 14:28:04 +0200

    FileSender tilføjet

---
+++ HiPi_PC/FileSender/FileSender/Server/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Server
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var server = new Server();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
