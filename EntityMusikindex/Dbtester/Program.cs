1e2795a06da7083f935145f2b1a71cdd5b347ff9
Christian Malmos <chriller855@gmail.com>
2013-05-03 17:11:30 +0200

    der var ikke brug for dem der de har deres egen
    jeg vil meget gerne have at jakob selv fikser denne

--- EntityMusikindex/Dbtester/Program.cs
+++ EntityMusikindex/Dbtester/Program.cs
@@ -14,26 +14,6 @@ namespace Dbtester
         {
             var dbh = new dbclases.Dbhandel();
 
-            //dbh.FillIP("192.168.001.190");
-
-            //List<string> ha = new List<string>();
-
-            //ha.Add("dancfdfe1");
-            //ha.Add("hafdfdps1");
-
-            //dbh.FillPath(ha);
-
-            //List<string> haps = new List<string>();
-
-            //haps.Add("js4j4s");
-
-            //dbh.Addgenre(haps);
-
-            //haps.Add("js4j4fdsfs");
-
-            //dbh.addArtist(haps);
-            //dbh.addAlbum(haps);
-
             var index = new FileIndexer(@"C:\Users\Public\Music\Sample Music");
 
             List<IMetadataReader> mdata = index.GetMetaData();
--- EntityMusikindex/TemplateSync/SyncTemplate.cs
+++ EntityMusikindex/TemplateSync/SyncTemplate.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MetaReader.FileIndexer;
 using MetaReader.MetadataReader;
+using dbclases;
 
 
 namespace TemplateSync
@@ -20,19 +21,24 @@ namespace TemplateSync
 
 
        public SyncTemplate(string absPath)
-        {
+       {
+           var db = new Dbhandel();
            //Creating Path's
             _AbsPath = absPath;
 
            _RelPath = MakeRelpathFromAbspath(absPath);
 
-           FillIP(); 
+           CreateStream();// RelPath is path from Live555 to Musicfolder
 
-           FillPath();
+           db.FillIP("192.168.001.190"); // denne skal hentes fra live555
           
-           FillDatabase();
+         
+        }
 
-            CreateStream();// RelPath is path from Live555 to Musicfolder
+        public void Sync()
+        {
+            //denne skal synce alle data
+
         }
 
         private string MakeRelpathFromAbspath(string a)
@@ -53,98 +59,6 @@ namespace TemplateSync
             return relativePath;
         }
 
-        public void FillDatabase()
-        {
-            var index = new FileIndexer(_AbsPath);
-
-            List<IMetadataReader> mdata = index.GetMetaData();
-
-            var nummer = new musikdata();
-           
-
-            using (var musik = new musikindexEntities1())
-            {
-
-
-                foreach (var metadata in mdata)
-                {
-                    nummer.Title = metadata.Title;
-                    nummer.Artist = metadata.Artist;
-                    nummer.Album = metadata.Album;
-                    nummer.Genre = metadata.Genre;
-                    nummer.NrLenth = metadata.LengthS;
-                    nummer.FileName = metadata.ItemName;
-                    nummer.FilePath_idFilePath = _RelPath;
-
-                    musik.musikdatas.Add(nummer);
-
-                    musik.SaveChanges();
-                }
-                
-
-            
-
-            }
-
-        }
-
-        public void FillIP()
-        {
-            using (var musik = new musikindexEntities1())
-            {
-                
-
-                string host = Dns.GetHostName();
-                IPHostEntry IPHost = Dns.GetHostEntry(Dns.GetHostName());
-
-                var myIP = new ip();
-
-                //myIP.idIP = IPHost.AddressList[0].ToString();
-                myIP.idIP = "192.168.001.090";
-
-                var ip = (from p in musik.ips select p.idIP);
-
-                bool chek =false;
-
-                foreach (var i in ip)
-                {
-                    chek = i.ToString() == myIP.idIP ? true : false;                    
-                }
- 
-
-                if(!chek)
-                {
-                    myIP.Owner = Environment.UserName;
-
-                    myIP.Protocol = "rtsp://";
-
-                    musik.ips.Add(myIP);
-
-                    musik.SaveChanges();
-                }
-               
-            }
-
-        }
-
-        public void FillPath()
-        {
-
-            var sti = new filepath();
-
-            sti.IP_idIP = "192.168.001.090";//egen ip getter skal laves som funktion
-            sti.idFilePath = _RelPath;
-            //_index = sti.idFilesti;
-
-            using (var musik = new musikindexEntities1())
-            {  
-                musik.filepaths.Add(sti);
-                               
-                musik.SaveChanges();
-                
-            }
-
-        }
 
         public void CreateStream()
         {
