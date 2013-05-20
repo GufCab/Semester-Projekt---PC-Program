e3b2ef82483157af02c51172c70c98d394f30dc6
Christian Malmos <chriller855@gmail.com>
2013-05-20 14:10:57 +0200

    Nu er observerble til gui lavet

---
+++ HiPi_PC/Containers/MusicIndexToGui.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace Containers
+{
+    class MusicIndexToGui : ObservableCollection<ITrack>
+    {
+        public void UpdateMusicindex(List<ITrack> listen)
+        {
+            this.Clear();
+
+            foreach (var track in listen)
+            {
+                Add(track);
+            }
+
+        }
+    }
+}
