4f46fcc3a734e8343c3d229964ad5f673ed17940
Christian Malmos <chriller855@gmail.com>
2013-05-31 01:11:40 +0200

    added comments

--- HiPi_PC/Database/PCDBModel.Context.cs
+++ HiPi_PC/Database/PCDBModel.Context.cs
@@ -15,6 +15,12 @@ namespace Database
     
     public partial class pcindexEntities : DbContext
     {
+        /// <summary>
+        ///  This code was generated from a template.
+        ///
+        ///    Manual changes to this file may cause unexpected behavior in your application.
+        ///    Manual changes to this file will be overwritten if the code is regenerated.
+        /// </summary>
         public pcindexEntities()
             : base("name=pcindexEntities")
         {
